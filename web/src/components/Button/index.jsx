4c3fd462598db7bdd933f6259f5b70f645b7ec0b
KovaKreative <eric.kovalevskyy@gmail.com>
2023-08-29 16:18:42 -0400

    Implement Tailwind

---
+++ web/src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+export default function Button(props) {
+  return (
+    <button className="w-1/3 h-12 rounded-xl bg-yellow-300 text-purple-700 text-xl font-bold hover:bg-yellow-200 hover:text-purple-500 active:bg-yellow-400 active:text-purple-700  transition">
+      {props.text || 'Button'}
+    </button>
+  )
+}
--- web/src/components/Home/index.jsx
+++ web/src/components/Home/index.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import Button from "../Button";
+
+import axios from "axios";
+
 export default function Home() {
+
+  useEffect(() => {
+    axios.get('/')
+      .then(res => {
+        console.log(res);
+      });
+  }, []);
+
   return (
     <main>
-      <h1>HOME</h1>
+      <h1 className="text-6xl text-yellow-300 font-bold mb-5">KovaKreative Geek Trivia Quiz</h1>
+      <Button text="Play" />
     </main>
-  )
+  );
 }

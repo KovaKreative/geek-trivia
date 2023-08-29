27a7ed624131ae2ec7f62b1feefb8a294cd96b74
KovaKreative <eric.kovalevskyy@gmail.com>
2023-08-29 11:27:20 -0400

    Install and set up backend requirements

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// require('dotenv').config();
+import { config } from 'dotenv';
+
+import express from 'express';
+import morgan from 'morgan';
+import cookieSession from 'cookie-session';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieSession({ name: 'session', keys: [process.env.KEY], maxAge: 365 * 24 * 60 * 60 * 1000 }));
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+  credentials: true
+}));
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});

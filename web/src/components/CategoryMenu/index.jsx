aa057b7f9200d6596974e02c96cef89f2153d040
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-01 14:02:05 -0400

    Refactor components

--- web/src/components/CategoryMenu/index.jsx
+++ web/src/components/CategoryMenu/index.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setCategory } from "../../features/quiz/quizSlice";
 import { goTo } from "../../features/views/viewSlice";
 
 import Button from "../Button";
 
-import axios from "axios";
-
 export default function CategoryMenu() {
 
-  const [categories, setCategories] = useState([]);
+  const categoriesState = useSelector(state => state.quiz.categories);
 
   const dispatch = useDispatch();
 
@@ -18,23 +15,9 @@ export default function CategoryMenu() {
     dispatch(goTo("QUIZ"));
   };
 
-  useEffect(() => {
-    // Get quiz from server
-    if (!categories.length) {
-      axios.get('http://localhost:3000/quiz/',
-        {
-          proxy: {
-            host: 'http://localhost',
-            port: '3000'
-          }
-        })
-        .then(res => {
-          setCategories(res.data);
-        });
-    }
-  }, []);
 
-  const quizButtons = categories.map((cat, i) => {
+
+  const quizButtons = categoriesState.map((cat, i) => {
     return <Button text={cat.category} key={i} onClick={() => goToQuiz(cat.id)}></Button>;
   });
 
--- web/src/components/Home/index.jsx
+++ web/src/components/Home/index.jsx
@@ -1,15 +1,43 @@
-import CategoryMenu from "../CategoryMenu";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { useSelector } from "react-redux";
+import { setCategories } from "../../features/quiz/quizSlice";
+
+import CategoryMenu from "../CategoryMenu";
+import Loader from "../Loader";
+
+import axios from 'axios';
 
 export default function Home() {
 
-  const categories = useSelector(state => state.quiz.categories);
+  const dispatch = useDispatch();
+
+  const categoriesState = useSelector(state => state.quiz.categories);
+
+  useEffect(() => {
+    console.log(categoriesState);
+  }, [categoriesState]);
+
+  useEffect(() => {
+    // Get categories from server
+    if (!categoriesState.length) {
+      axios.get('http://localhost:3000/quiz/',
+        {
+          proxy: {
+            host: 'http://localhost',
+            port: '3000'
+          }
+        })
+        .then(res => {
+          dispatch(setCategories(res.data));
+        });
+    }
+  }, []);
 
   return (
     <section className="Home">
       <h1 className="text-6xl text-yellow-300 font-bold mb-5">KovaKreative Geek Trivia Quiz</h1>
-      <CategoryMenu />
+      {categoriesState.length ? <CategoryMenu /> : <Loader />}
     </section>
   );
 }

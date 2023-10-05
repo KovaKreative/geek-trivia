75cb4e82312dec6938577555635ad32a83ccd6c8
KovaKreative <eric.kovalevskyy@gmail.com>
2023-10-05 15:57:03 -0400

    Style answer page

--- web/src/components/Results/index.jsx
+++ web/src/components/Results/index.jsx
@@ -17,21 +17,24 @@ export default function Results() {
     dispatch(goHome());
   };
 
-  const renderedResults = questions.map(q => {
+  const renderedResults = questions.map((q, i) => {
     const correct = q.answer === q.correctAnswer;
     return <figure key={q.id}>
-      <p className="text-yellow-300 text-2xl">{q.question}</p>
+      <p className="text-yellow-300 text-2xl">{i+1}: {q.question}</p>
       <p className={correct ? "text-green-300 text-3xl" : "text-red-500 text-3xl"}>You answered: {q.answer}</p>
     </figure>;
   });
 
   return (
     <>
-      <h1 className="text-5xl text-yellow-300 font-bold mb-5">{correct} out of {questions.length}</h1>
-      <div className="bg-indigo-950 p-5 mb-4 flex gap-2 flex-wrap justify-center content-center h-1/4">
+      <h2 className="text-4xl text-yellow-300 font-bold mb-5">{correct} out of {questions.length}</h2>
+      <h1 className="text-5xl text-yellow-300 font-bold mb-5">{Math.round(correct/questions.length*100)}% Correct</h1>
+      <div className="bg-indigo-950 p-5 mb-4 flex gap-2 flex-wrap justify-center overflow-y-scroll">
         {renderedResults}
       </div>
+      <footer className="w-full mb-4">
       <Button text="Play Again" onClick={resetGame} />
+      </footer>
     </>
   );
 }
